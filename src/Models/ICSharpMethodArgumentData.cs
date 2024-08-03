936782d1f2aba3950d320fdf73d449fc74173176
ErikWe <erikwestberg7@gmail.com>
2024-08-03 03:43:39 +0200

    Initial implementation

---
+++ src/Models/ICSharpMethodArgumentData.cs
@@ -0,0 +1,10 @@
+﻿namespace Paraminter.Arguments.CSharp.Method.Models;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+/// <summary>Represents syntactic data about a C# method argument.</summary>
+public interface ICSharpMethodArgumentData
+{
+    /// <summary>The syntactic data about the C# method argument.</summary>
+    public abstract ArgumentSyntax Argument { get; }
+}
